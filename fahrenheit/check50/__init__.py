0a211189ee6f0ef56318acd98e6a4ee8143ca22a
Angela Armstrong <aarmstrong@rocklandschools.org>
2018-02-07 15:22:12 -0500

    Update __init__.py

--- fahrenheit/check50/__init__.py
+++ fahrenheit/check50/__init__.py
@@ -12,7 +12,7 @@ class Fahrenheit(Checks):
     """fahrenheit.py compiles"""
     self.spawn("clang -o fahrenheit fahrenheit.py -lcs50 -lm).exit(0)
     
-  @check("compiles)
+  @check("compiles")
   def test_neg40_(self)
     """Input of -40 yeilds output of 4"""
     self.spawn("python fahrenheit.py").stdin("-40").stout("-40", "-40").exit(0)
