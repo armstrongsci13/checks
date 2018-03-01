8f924b0190b208d0df1ac864c02d711f56d543dd
Angela Armstrong <aarmstrong@rocklandschools.org>
2018-03-01 13:48:23 -0500

    Update __init__.py

--- the/check50/__init__.py
+++ the/check50/__init__.py
@@ -1,1 +1,13 @@
-v
+from check50 import *
+
+class the(Checks):
+
+  @check()
+  def exists(self):
+    """the.py exists"""
+    self.require("the.py")
+  
+  @check("exists")
+  def compiles(self):
+    """the.py compiles"""
+    self.spawn("clang -o the the.py -lcs50 -lm).exit(0)
