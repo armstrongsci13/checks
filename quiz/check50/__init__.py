c8192861c9fc48d66138002358d9d0ef19e7b889
Angela Armstrong <aarmstrong@rocklandschools.org>
2018-03-21 10:50:54 -0400

    Update __init__.py

--- quiz/check50/__init__.py
+++ quiz/check50/__init__.py
@@ -1,1 +1,13 @@
+from check50 import *
 
+class quiz(Checks):
+
+  @check()
+  def exists(self):
+    """quiz.py exists"""
+    self.require("quiz.py")
+  
+  @check("exists")
+  def compiles(self):
+    """quiz.py compiles"""
+    self.spawn("clang -o quiz quiz.py -lcs50 -lm).exit(0)
